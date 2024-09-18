f2387ba1918ad9998769cb22093a9a505e688b00
ahmadghani27 <149065917+ahmadghani27@users.noreply.github.com>
2024-09-18 11:50:52 +0000

    1

--- percobaan3.java
+++ percobaan3.java
@@ -1,17 +1,18 @@
 public class percobaan3 {
     public static void main(String[] args) {
-        int data2[][]={{4,6,4,2,8,4,2,11},{4,6,4,2,8,4,2,10}};
-        int o=0;
-        // While
-        while (o<data2.length) {
-            int oo=0;
-            while (oo<data2[o].length) {
-                System.out.print(data2[o][oo]+" ");
+        int data2[][] = {{4, 6, 4, 2, 8, 4, 2, 11}, {4, 6, 4, 2, 8, 4, 2, 10}};
+        int o = 0;
+
+        while (o < data2.length) {
+            int oo = 0;
+            while (oo < data2[o].length) {
+                System.out.print(data2[o][oo] + " ");
                 oo++;
             }
             o++;
             System.out.println("\n");
         }
+
         o = 0;
         do {
             int oo = 0;
@@ -20,7 +21,7 @@ public class percobaan3 {
                 oo++;
             }
             o++;
-            System.out.println("\n");  
+            System.out.println("\n");
         } while (o < data2.length);
     }
 }
