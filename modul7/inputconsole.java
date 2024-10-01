0c6f86d1cf5f019ae39dd7b62656af89d47cc99e
ahmadghani27 <149065917+ahmadghani27@users.noreply.github.com>
2024-10-01 00:18:12 +0000

    tugas

--- modul7/inputconsole.java
+++ modul7/inputconsole.java
@@ -19,4 +19,16 @@ public class inputconsole {
     public static double readDouble(){
         return Double.parseDouble(readString());
     }
+    public static void main(String args[]){
+        System.out.print("data1 = ");
+        inputconsole ic = new inputconsole();
+        int data1 = ic.readInt();
+        
+        System.out.print("data2 = ");
+        int data2 = ic.readInt();
+        
+        int data3 = data1 + data2;
+        System.out.println("data1 + data2 = " + data3);
+    }
 }
+
