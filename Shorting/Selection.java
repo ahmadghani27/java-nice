9153de4d99c3cf50907de04a0eca5f31103490af
ahmadghani27 <149065917+ahmadghani27@users.noreply.github.com>
2024-10-15 10:49:18 +0000

    Selection shorting(not yet)

---
+++ Shorting/Selection.java
@@ -0,0 +1,25 @@
+public class selection {
+    public static void main(String[] args) {
+        int[] tabel = { 2, 4, 3, 1, 6, 7, 4, 9, 4, 8 };
+        int p, k, j;
+        p = tabel.length;
+        for (k = 0; k < p; k++) {
+            int min = tabel[k];
+            int pos = k;
+            for (j = k; j < p; j++) {
+                if (tabel[j] < min) {
+                    min = tabel[j];
+                    pos = j;
+                }
+            }
+            if (k != pos) {
+                int temp = tabel[k];
+                tabel[k] = tabel[pos];
+                tabel[pos] = temp;
+            }
+        }
+        for (k = 0; k < tabel.length; k++) {
+            System.out.print(tabel[k] + " ");
+        }
+    }
+}
