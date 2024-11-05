600887f4a7322f72921780621302878411b4b5f2
ahmadghani27 <149065917+ahmadghani27@users.noreply.github.com>
2024-11-05 13:26:46 +0000

    Fisrt Try

--- Listrik/MenuUtama.java
+++ Listrik/MenuUtama.java
@@ -13,21 +13,31 @@ public class MenuUtama extends JFrame {
 
   public MenuUtama() {
     setTitle("Listrik - iAN Konter");
-    setSize(640, 400);
+    setSize(640, 450);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setResizable(false);
     setLayout(null);
     setVisible(true);
     Dimension window = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (window.width - 640) / 2; 
-    int y = (window.height - 400) / 2; 
+    int y = (window.height - 450) / 2; 
     setLocation(x, y);
 
     JLtitle = new JLabel("iAN Konter");
-    setBounds(x, y, x, y);
+    JLtitle.setBounds(20, 370, 180, 50);
+    JLtitle.setFont(new Font("SansSerif", 3, 28));
+    JLtitle.setForeground(new Color(0, 51, 153));
+    JLtitle.setText("iAN Konter");
+    add(JLtitle);
   }
 
-  public void InputID(){
+  public void InputID(){    
+    JLinputID = new JLabel("Masukkan ID Meteran Listrik");
+    JLinputID.setBounds(120, 40, 400, 50);
+    JLinputID.setFont(new java.awt.Font("SansSerif", 1, 28)); 
+    JLinputID.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+    JLinputID.setText("Masukkan ID Meteran Listrik");
+    add(JLinputID);
 
   }
 
